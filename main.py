1d2bc1aec48dc0b3b9f328f65bdb272f9700419c
aleks <aleks_shura@mail.ru>
2023-06-07 12:29:26 +0200

    исправление

--- main.py
+++ main.py
@@ -1,34 +1,70 @@
+import ssl
+
 import requests
 import pandas as pd
+import urllib3 as urllib3
 from bs4 import BeautifulSoup
 from zeep import Client
 from zeep.transports import Transport
 
+code = 'lf-2179'
+brand = 'Zekkert'
+stock = 'HST228'
+count = '1'
+
 connect = {
-            'wsdl': 'http://api.rossko.ru/service/v2.1/GetOrders',
-            'options': {
-                'transport': Transport(timeout=1),
-            }
-        }
+    'wsdl': 'http://api.rossko.ru/service/v2.1/GetCheckout',
+    'options': {
+        'transport': Transport(timeout=1),
+    }
+}
 
 param = {
-            'KEY1': 'aae7b144f41c0ef7d840bc948de6dbbc',
-            'KEY2': '24e09553d43de0243375642f07efba6d',
-            'limit': '50'
+    'KEY1': 'aae7b144f41c0ef7d840bc948de6dbbc',
+    'KEY2': '24e09553d43de0243375642f07efba6d',
+    'delivery': {
+        'delivery_id': '000000002',
+        'address_id': '144623'
+    },
+    'payment': {'payment_id': '1',
+                'requisite_id': ''
+                },
+    'contact': {
+        'name': 'Олег',
+        'phone': '+74012710517'
+    },
+    'delivery_parts': True,
+    'PARTS': {
+        'Part':
+            {
+                'partnumber': code,
+                'brand': brand,
+                'stock': stock,
+                'count': '1'
+            }
         }
+}
 
 client = Client(connect['wsdl'], transport=connect['options']['transport'])
-result = client.service.GetOrders(**param)
+try:
+    result = client.service.GetCheckout(**param)
+    print(result)
+except Exception as e:
+    print('Ошибка', e)
 
-dict_order = {}
-all_items = result['OrdersList']['Order']
-
-for item in all_items:
-    parts = item['parts']['part']
-
-    for part in parts:
-        code = part['partnumber']
-        quantity = part['count']
-        dict_order[code] = quantity
-
-print(dict_order)
+# connect = {
+#             'wsdl': 'http://api.rossko.ru/service/v2.1/GetCheckoutDetails',
+#             'options': {
+#                 'transport': Transport(timeout=1),
+#             }
+#         }
+#
+# param = {
+#             'KEY1': 'aae7b144f41c0ef7d840bc948de6dbbc',
+#             'KEY2': '24e09553d43de0243375642f07efba6d',
+#
+#         }
+#
+# client = Client(connect['wsdl'], transport=connect['options']['transport'])
+# result = client.service.GetCheckoutDetails(**param)
+# print(result)
